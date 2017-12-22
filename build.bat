7abc1e87d0d1b7a1d6b74f0efc3a5c1a7fb4fc87
Thomas Pulzer <t.pulzer@kniel.de>
2017-12-22 12:18:05 +0100

    Initial commit for the project Build the .msi from the Greenshot installable files.

---
+++ build.bat
@@ -0,0 +1,2 @@
+"c:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" "Greenshot 1.2.10.6.wxs"
+"c:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" "Greenshot 1.2.10.6.wixobj" -cultures:en-us -loc GreenshotUI_en-us.wxl -ext WixNetFxExtension -out "Greenshot 1.2.10.6.msi"
